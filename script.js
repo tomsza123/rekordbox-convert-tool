1b6ecb61723191341270e58191bb0a2a7667d355
tomsza123 <szablak.tomek@gmail.com>
2020-01-23 11:51:26 +0100

    Add files via upload
    Na chwilę obecną konwersja playlist z kolekcji zawartej w pliku rekordbox.xml do playlist w formacie .m3u przechodzi pomyślnie. Wynikiem końcowym skryptu jest plik .zip stworzony przez bibliotekę JSZip, w którym znajdują się playlisty.  TO DO: interfejs graficzny z możliwością wyboru pliku .xml oraz uwzględnienie hierarchii folderów i podfolderów znajdującej się w pliku .xml(na chwilę obecną wszystkie playlisty wrzucane są do jednego folderu).

--- script.js
+++ script.js
@@ -11,38 +11,44 @@ let getXMLFile = function(path, callback){
 };
 
 getXMLFile("rekordbox.xml", function(xml){
-//potrzebny nodejs
-    let track = xml.all[2].getElementsByTagName("TRACK");
-   /*  console.log(track.length)
-    console.log(track[0]);    
-    console.log(track[0].getAttribute("TrackID"));
-    console.log(track[0].getAttribute("Name"));
-    console.log(track[0].getAttribute("Location"));
-    let playlist = xml.all[].getElementsByTagName("PLAYLISTS");
-    console.log(playlist) */
+
+    let track = xml.all[2].getElementsByTagName("TRACK");  
     let node = xml.all[0].getElementsByTagName("NODE");
     let plTrack = xml.all[0].getElementsByTagName("TRACK");
-    console.log(plTrack);
     let i = 1;
     let p = 0;
+    let replacement = '\\';
+    let count = 1;
+    let folder = "root\\";
+
+    var zip = new JSZip();
  
-    while(i<node.length-120){
-        if(node[i].getAttribute("Type") == "0"){
-            console.log("Folder: " + node[i].getAttribute("Name"));
+    while(i<node.length){
+        if(node[i].getAttribute("Type") == "0"){ 
+            let folder = "root\\"+node[i].getAttribute("Name")+"\\";
+            console.log("Folder: " + folder);
         }
         else if(node[i].getAttribute("Type") == "1"){
-            console.log("    Playlist: " + node[i].getAttribute("Name"));
-            console.log("#EXTM3U")
+
+            let name = node[i].getAttribute("Name");
+            let playlist = "#EXTM3U";
             for(let o = 0;o<parseInt(node[i].getAttribute("Entries"));o++){
                 let tid = plTrack[track.length+p].getAttribute("Key");
                 p++;
-                for(a = 0;a<track.length;a++){//chyba dodac wykonawce
-                    if(tid == track[a].getAttribute("TrackID")){
-                        console.log("#EXTINF:"+track[a].getAttribute("TotalTime")+","+track[a].getAttribute("Name")+"\n"+track[a].getAttribute("Location"))
+                for(a = 0;a<track.length;a++){
+                    if(tid == track[a].getAttribute("TrackID")){                        
+                        let location = (track[a].getAttribute("Location")).substr(17).replace(/\//g, replacement);
+
+                        playlist += ("\n#EXTINF:"+track[a].getAttribute("TotalTime")+","+track[a].getAttribute("Name")+" - "+track[a].getAttribute("Artist")+"\n"+decodeURIComponent(location))
                     }
-                }
+                } 
             }
+
+            zip.file(/* folder+ */name.replace(/\//g, "-") + ".m3u", playlist);//nazwy plikow nie moga zawierac slashy, skrypt metodą replace podmienia je na myślniki
         }        
         i++;
-    } 
+    }
+    zip.generateAsync({type:"blob"}).then(function(content) {
+        saveAs(content, "rekordbox.zip");
+    }); 
 })
