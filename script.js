23d1c76a6c85f8dacbbc4568864567dc52ceb3d6
tomsza123 <szablak.tomek@gmail.com>
2020-04-19 20:06:50 +0200

    Add files via upload
    rozpocząłem pracę nad konwersją xml z rekordboxa 5 do nml traktora. Na razie bez przelatywania po całej kolekcji a wynik zamiast zapisać się do nml-a wyśiwetla się w konsoli.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-/*
+﻿/*
 todo:
 1.zrobić funkcję makePlaylist()
 2.poprzenosic powtarzajace sie zmienne do sekcji globalnej
-3.uwzględnić hierarchię folderów przy eksporcie playlist do zipa
-4.zaprojektować interfejs graficzny z mozliwoscia wyboru pliku xml
-5.napisać konwersję xml rekordboxa do nml traktora
+3.napisac funkcjonalnosc usuwajaca duplikaty w playliscie
+4.uwzględnić hierarchię folderów przy eksporcie playlist do zipa
+5.zaprojektować interfejs graficzny z mozliwoscia wyboru pliku xml
+6.napisać konwersję xml rekordboxa do nml traktora(i na odwrót)
+7.dodac funkcjonalność usuwającą fizycznie z dysku utworu zgromadzone w konkretnej playliscie(np. kosz)
+8.funkcja generująca raport o ilości utworów w każdej playliście
+9.dodac mozliwosc przegladania wybranych playlist(lub pomijania inteligentnych)
+10.wyszukiwanie brakujacych utworów
 */
+
+let xmlFile = 'rekordbox.xml';
+
 let getXMLFile = function(path, callback){
     let request = new XMLHttpRequest();
     request.open("GET",path);
@@ -101,4 +109,35 @@ function tracksWithoutPlaylist(xmlFile){
         var blob = new Blob([playlist], {type:"text/plain;charset=utf-8"});
         saveAs(blob,"rb_not_exist_in_playlists.m3u"); 
     }); 
+}
+
+function convertRbtoTrk(xmlFile){
+    getXMLFile(xmlFile, function(xml){
+        let track = xml.all[2].getElementsByTagName("TRACK");  
+        let node = xml.all[0].getElementsByTagName("NODE");
+        let d = new Date();
+        
+        
+
+        console.log(track[1]);
+        //deklaracja nml-a
+        console.log('<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\n<NML VERSION="19"><HEAD COMPANY="www.native-instruments.com" PROGRAM="Traktor"></HEAD>\n  <MUSICFOLDERS></MUSICFOLDERS>\n    <COLLECTION ENTRIES="'+track.length+'">');
+        //dodawanie utworu do kolekcji(konwersja z rb do trk) 
+        let location=track[1].getAttribute("Location");
+        location = decodeURIComponent(location);
+        let location_folders = location.substring(19,location.lastIndexOf("/"));
+        location_folders = location_folders.replace(/\//g,"/:");
+        let location_filename = location.substring(location.lastIndexOf("/")+1)
+        let location_volume = location.substring(17,19);
+        //sprawdzic jaka ma byc wartosc w VOLUMEID
+        //zobaczyć jaka wartosc ma być w MODIFIED_TIME
+        //FLAGS
+        //sprawdzić FILESIZE dla pewnosci (czy mb czy mib)
+        //dodać import cue
+        //zobaczyć musical_key
+        console.log('<ENTRY MODIFIED_DATE='+d.getFullYear()+'/'+(d.getMonth()+1)+'/'+d.getDate()+' MODIFIED_TIME="'+d.getTime()+' TITLE="'+track[1].getAttribute("Name")+'" ARTIST="'+track[1].getAttribute("Artist")+'" <LOCATION DIR="'+location_folders+'/:"'+' FILE="'+location_filename+'" VOLUME="'+location_volume+'" VOLUMEID="dc962188></LOCATION><MODIFICATION_INFO AUTHOR_TYPE="user"></MODIFICATION_INFO><INFO BITRATE="'+track[1].getAttribute("BitRate")+'000'+'" GENRE="'+track[1].getAttribute("Genre")+'" KEY="'+track[1].getAttribute("Tonality")+'" PLAYTIME="'+track[1].getAttribute("TotalTime")+'" IMPORT_DATE="'+track[1].getAttribute("DateAdded").replace(/-/g,"/")+'" RELEASE_DATE="'+track[1].getAttribute("Year")+'/1/1" FILESIZE="'+track[1].getAttribute("Size")+'"></INFO>\n<TEMPO BPM='+track[1].getAttribute("AverageBpm")+'0000" BPM_QUALITY="100.000000"></TEMPO>\n</ENTRY>');
+
+        console.log('</COLLECTION>\n</NML>')
+    }); 
+
 }
